14cb420267309f4480e892aeb9a45cec222bce26
ACatpinar <acatpinar@windowslive.com>
2020-07-13 21:59:12 -0400

    Day 02 Class

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -7,11 +7,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
         plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
         dryRun =false,
-        tags = "@librarian"
+        tags = "@loginWithBackGround"
+
+
 
 )
 
